bad1ed29989e83c49c0ba0067090887a98d226e5
berkejanos <berke_janos@yahoo.com>
2024-06-12 16:32:17 +0200

    mobile view done

--- landing.js
+++ landing.js
@@ -1,7 +1,7 @@
-const modalImageUrls=[
-'./images/desktop-image-hero-1.jpg',
-'./images/desktop-image-hero-2.jpg',
-'./images/desktop-image-hero-3.jpg'
+const modalImageUrls = [
+    './images/desktop-image-hero-1.jpg',
+    './images/desktop-image-hero-2.jpg',
+    './images/desktop-image-hero-3.jpg'
 ]
 
 const introductionTitle = [
@@ -14,7 +14,6 @@ const introductionText = [
     "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life.Create a room in your own style with our collection and make your property a reflection of you and what you love.",
     "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
     " Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office."
-
 ]
 
 let actualPage = 0;
@@ -28,11 +27,10 @@ function onLeftClick() {
     document.getElementById('modal').style.backgroundImage = "url('" + newImageUrl + "')";
     document.getElementById('int-title').innerHTML = introductionTitle[actualPage];
     document.getElementById('int-text').innerHTML = introductionText[actualPage];
-    console.log(newImageUrl);
 }
 
 function onRightClick() {
-    if (actualPage <2) {
+    if (actualPage < 2) {
         actualPage++;
     }
     else { actualPage = 0; }
@@ -40,5 +38,17 @@ function onRightClick() {
     document.getElementById('modal').style.backgroundImage = "url('" + newImageUrl + "')";
     document.getElementById('int-title').innerHTML = introductionTitle[actualPage];
     document.getElementById('int-text').innerHTML = introductionText[actualPage];
-    console.log(newImageUrl);
+}
+
+function changeMenu() {
+    const navbar = document.getElementById("navbar");
+    const overlay = document.getElementById("overlay");
+    if (navbar.className === "navbar") {
+        navbar.className += " responsive";
+        overlay.style.display = 'block';
+
+    } else {
+        navbar.className = "navbar";
+        overlay.style.display = 'none';
+    }
 }
